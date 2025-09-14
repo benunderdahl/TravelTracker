0c8a9cdd31bd1406083c1566762b98184b782bed
benunderdahl <benjamin.underdahl@proton.me>
2025-09-13 20:40:58 -0500

    added postgres server to project

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+import 'dotenv/config'
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+const db = new pg.Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB
+})
+db.connect()
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
 });
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
